c4a6e0511fc55c3c99c5f5d569d47b6ca228bf4e
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2018-06-29 14:51:35 -0700

    Solved reversedString problem

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    // input: str
+    // output: reversed str
+    // assumpt: use reverse method
+
+    return str.split('').reverse().join('');
+}
+
+module.exports = reverse;
