ca8c8719b58524382dfb29b964d985a692bd64a1
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2018-07-02 00:38:17 -0700

    Chunk solution 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // assumpt: get the last element of newArr
+      // if a last element size eq to size or last element is falsy then add [it] to newArr
+      // else push element to last element
+    let newArr = [];
+    for (var el of array) {
+      let lastEl = newArr[newArr.length - 1];
+      if (!lastEl || lastEl.length === size) {
+        newArr.push([el]);
+      } else {
+        lastEl.push(el)
+      }
+    }
+    return newArr;
+  }
+  
+
 
 module.exports = chunk;
