7bf9cbfc63931535d8b5a23047b78cea118784e4
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2018-07-25 19:37:50 -0700

    Pyramid recursive solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,20 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(num) {
-    let midpoint = Math.floor((num * 2 - 1) / 2)
-    for (let row = 0; row < num; row++) {
-      let str = '';
-      for (let column = 0; column < num * 2 - 1; column++) {
-        if (column <= midpoint + row && column >= midpoint - row) {
-          str += '#';
-        } else {
-          str += ' ';
+// function pyramid(num) {
+//     let midpoint = Math.floor((num * 2 - 1) / 2)
+//     for (let row = 0; row < num; row++) {
+//       let str = '';
+//       for (let column = 0; column < num * 2 - 1; column++) {
+//         if (column <= midpoint + row && column >= midpoint - row) {
+//           str += '#';
+//         } else {
+//           str += ' ';
     
-        }
-      }
-      console.log(str)
+//         }
+//       }
+//       console.log(str)
+//     }
+//   }
+
+// recursive solution
+
+function pyramid(num, row = 0, str = '') {
+    if (row === num) {
+      return;
     }
+  
+    if (str.length === num * 2 - 1) {
+      console.log(str)
+      return pyramid(num, row + 1);
+    }
+  
+    let midpoint = Math.floor((num * 2 - 1) / 2);
+    let add;
+    if (str.length >= midpoint - row && str.length <= midpoint + row) {
+      add = '#';
+    } else {
+      add = ' ';
+    }
+  
+    pyramid(num, row, str + add);
   }
 
 module.exports = pyramid;
