a2c553a9517fe531b030ae4819d95958315f61bd
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2018-07-26 21:47:42 -0700

    Solved Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let check = 'aeiou';
+    let count = 0;
+    for (let letter of str.toLowerCase()) {
+      if (check.includes(letter)) {
+        count++;
+      }
+    }
+    return count;
+}
 
 module.exports = vowels;
