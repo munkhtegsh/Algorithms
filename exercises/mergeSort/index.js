dea8106f38adac21f1c123f15d99990d8ac22558
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2018-08-24 23:57:49 -0700

    Another way to solve merge sort, but it has some serious issue

--- exercises/mergeSort/index.js
+++ exercises/mergeSort/index.js
@@ -3,24 +3,62 @@
 // --- Examples
 // mergeSort([1,2,3,4,5], [2,4,5,7]) => [1,2,3,4,5,7]
 
+// Solution: 1
+// function mergeSort(arr1, arr2) {
+//   // create new []
+//   // need iterator for each arrays
+//   // keep looping if idx is less than the arr 
+//   let newArr = [];
+//   let i = 0;
+//   let j = 0;
+//   while (i < arr1.length || j < arr2.length) {
+//     if (arr1[i] <= arr2[j]) {
+//       newArr.push(arr1[i])
+//       i++;
+//     } else {
+//       newArr.push(arr2[j]);
+//       j++;
+//     }
+//   }
+  
+//   return newArr;
+// }
+
+// Solution 2
 function mergeSort(arr1, arr2) {
-  // create new []
-  // need iterator for each arrays
-  // keep looping if idx is less than the arr 
-  let newArr = [];
-  let i = 0;
-  let j = 0;
-  while (i < arr1.length || j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      newArr.push(arr1[i])
-      i++;
+  // if either one of them empty arr then return other one
+  // take the first item in the arrays
+  // set the idx in each one of them
+  // check each one of them are true then loop
+    // check the 
+  
+  if (arr1.length === 0) {
+    return arr2;
+  }
+  
+  if (arr2.length === 0) {
+    return arr1;
+  }
+  
+  let aElm = arr1[0];
+  let bElm = arr2[0];
+  let i = 1;
+  let j = 1;
+  let mergedArr = [];
+  
+  while (aElm || bElm) {
+    if ((aElm && !bElm) || aElm < bElm) {
+      mergedArr.push(aElm);
+      aElm = arr1[i++];
     } else {
-      newArr.push(arr2[j]);
-      j++;
+      mergedArr.push(bElm);
+      bElm = arr2[j++];
     }
   }
   
-  return newArr;
+  return mergedArr;
 }
+
+
 
 mergeSort([1,2,3,4,5], [2,4,5,7]);
