60571c472695647fb0672811b3c8af79742891c2
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-09-12 23:58:03 -0700

    Finished mergeSort

--- exercises/mergeSort/index.js
+++ exercises/mergeSort/index.js
@@ -1,26 +1,35 @@
-// --- Directions
-// Given two arrays with integers, return a array with sorted integers
-// --- Examples
-// mergeSort([1,2,3,4,5], [2,4,5,7]) => [1,2,3,4,5,7]
-
-/// mergeSort([10,3,5,7,2,4,4,9]) => [2,3,4,4 5,7, 9,10]
-
-//divide it by two arr
-//recursively call the mergeSort
 
 function mergeSort(arr) {
   // iterate arr
   // var
+  if (arr.length === 1) {
+    return arr
+  }
   let medium = Math.floor(arr.length / 2);
   let firstHalf = arr.slice(0, medium);
   let secondHalf = arr.slice(medium);
-  return mergeSort(merge(firstHalf), merge(secondHalf));
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
 }
 
 // create merge fn 
 // iterate first one
 
 function merge(firstHalf, secondHalf) {
-  
+  let j = 0;
+  let i = 0;
+  let container = [];
+  while(i < firstHalf.length && j < secondHalf.length) {
+    if (firstHalf[i] < secondHalf[j]) {
+      container.push(firstHalf[i])
+      i++;
+    } else {
+      container.push(secondHalf[j])
+      j++;
+    }
+  }
+
+  return container.concat(firstHalf.slice(i), secondHalf.slice(j));
+; 
 }
 
+mergeSort([10,3,5,7,2,4,4,9])
