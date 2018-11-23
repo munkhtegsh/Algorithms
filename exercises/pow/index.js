6eda3941d4ee7aa6fd3044c0cff6c7f45294ef19
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-11-23 11:39:34 -0800

    pow() by Albert\'s theory not efficient

--- exercises/pow/index.js
+++ exercises/pow/index.js
@@ -31,3 +31,16 @@ function power(x, n) {
   // If n is negative, recursively compute x-n, so that the exponent becomes positive. Then, xn = 1/x-n.
   return 1/x * power(x, n + 1)
 }
+
+// Albert's way
+// function power(x, n) {
+//   if (n === 0) {
+//     return 1;
+//   } else {
+//     if (n > 0) {
+//       return x * power(x, n - 1)
+//     } else {
+//       return 1/x * power(x, n + 1)
+//     }
+//   }
+// }
