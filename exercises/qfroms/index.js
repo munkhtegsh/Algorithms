2fe2b8971ce0e932b64555a4dd48e052f12c218e
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2019-01-09 15:01:33 -0800

    Solution qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,42 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.s1 = new Stack();
+    this.s2 = new Stack();
+  }
+  add(el) {
+    this.s1.push(el);
+  }
+
+  remove() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop());
+    }
+
+    let record = this.s2.pop();
+
+    while (this.s2.peek()) {
+      this.s1.push(this.s2.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.s1.peek()) {
+      this.s2.push(this.s1.pop());
+    }
+
+    let peekResult = this.s2.peek();
+
+    while (this.s2.peek()) {
+      this.s1.push(this.s2.pop());
+    }
+
+    return peekResult;
+  }
+}
 
 module.exports = Queue;
